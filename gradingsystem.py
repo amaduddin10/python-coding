ed4ed8b46ef864f5f08aba7bae83427026e8cf66
amaduddin10 <arifm9241@gmail.com>
2026-02-07 18:24:19 +0100

    Add files via upload

---
+++ gradingsystem.py
@@ -0,0 +1,22 @@
+mark=int(input('enter your mark:'))
+
+if mark < 50:
+    print('grade f')
+
+elif 51 <= mark <= 70:
+    print('grade d')
+
+elif 51 <= mark <= 70:
+    print('grade c')
+
+elif 71 <= mark <=80:
+    print('grade b')
+
+elif 81 <= mark <= 90:
+    print('grade a')
+
+elif 91 <= mark <= 100:
+    print('grade a+')
+
+else:
+    print('invalid mark')
