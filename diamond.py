ca9f1e60ec2767d28859212f58401e54dfc92d4a
amaduddin10 <arifm9241@gmail.com>
2026-02-28 18:33:00 +0100

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,22 @@
+row = int(input('enter a number of rows:'))
+
+if row % 2==0:
+    half = row// 2
+else:
+    half = row// 2 + 1
+
+space=half - 1
+
+for i in range(1, half +1):
+    x = ' ' * space
+    y = 'x' * ( 2 * i - 1)
+    print(x+y)
+    space=space-1
+
+space = 1
+
+for i in range(half - 1, 0, -1):
+    x = ' ' * space
+    y = 'x' * ( 2 * i - 1)
+    print(x+y)
+    space=space+1
